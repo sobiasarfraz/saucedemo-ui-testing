453dcfade8bdf83b72e68a03f9880029c969c990
sobiasarfraz <moon_202119@yahoo.com>
2025-09-04 08:04:26 -0400

    update logging helper.py

--- logging_helper.py
+++ logging_helper.py
@@ -4,7 +4,7 @@ import logging
 
 def setup_logging():
     os.makedirs("logs", exist_ok=True)  # make log folder if not created
-    filename = f"logs/log_{datetime.now().strftime("%y-%m-%d_%H-%M-%S")}.log"  # log path with timestamp, now() access current time & strftime() format that time
+    filename = f"logs/log_{datetime.now().strftime('%y-%m-%d_%H-%M-%S')}.log"  # log path with timestamp, now() access current time & strftime() format that time
     logging.basicConfig(
         filename=filename,
         level=logging.INFO,
